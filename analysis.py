d822d7f556cf8531ae456d229681d5f96144f80e
Munster2020 <g00387820@gmit.ie>
2020-04-05 17:21:51 +0100

    Added code section that outputs summary of each variables

--- analysis.py
+++ analysis.py
@@ -1,8 +1,14 @@
 # importing dataset using pandas
 # verifying the imported dataset
-
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-# names = ['sepal length','sepal width','petal length','petal width','class'])
-dataset = pd.read_csv('iris.csv')
-dataset.describe()
-print (dataset.shape)
+
+iris_df = pd.read_csv('iris.csv')
+f = iris_df['sepal length in cm'].describe()
+g = iris_df['sepal width in cm'].describe()
+h = iris_df['petal length in cm'].describe()
+i = iris_df['petal width in cm'].describe()
+j = iris_df['class'].describe()
+
+print(f, g, h, i, j)
