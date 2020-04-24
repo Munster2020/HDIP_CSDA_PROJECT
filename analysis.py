20dfac9bb1662d98e01a92735a75af6f096305f0
Munster2020 <g00387820@gmit.ie>
2020-04-24 20:30:54 +0100

    Updates

--- analysis.py
+++ analysis.py
@@ -16,10 +16,11 @@ import sys
 # Reads the contents of 'iris.csv' dataset and create column headings
 # See Readme for source and file description.
 data = pd.read_csv('iris.csv')
-iris_df = pd.DataFrame(data, columns = ['sepal length in cm', 'sepal width in cm', 'petal length in cm', 'petal width in cm', 'class'])
+iris_df = pd.DataFrame(data, columns=[
+                       'sepal length in cm', 'sepal width in cm', 'petal length in cm', 'petal width in cm', 'class'])
 
 # Output a summary of the dataset and each variable to text file called "Summary"
-#sys.stdout = open("iris_summary.txt", 'w')
+sys.stdout = open("iris_summary.txt", 'w')
 
 # This section explores the dataset
 print('===============')
@@ -40,37 +41,37 @@ print('--------------------------')
 print(iris_df.info())
 
 # The next section generates descriptive statistics on the dataset
-print ('===================================')
-print ('Statistical Summary of numeric data')
-print ('-----------------------------------')
-print (iris_df.describe())
-print ('=======================================')
-print ('Statistical Summary of non-numeric data')
-print ('---------------------------------------')
-print (iris_df.describe(include=[object]))
-print ('===============')
-print ('Dataset Median')
-print ('--------------')
-print (iris_df.median())
-print ('===============')
-print ('Dataset Mode')
-print ('--------------')
-print (iris_df.mode(numeric_only=True))
-print ('===============')
-print ('Dataset Variance')
-print ('--------------')
-print (iris_df.var())
-print ('====================================')
-print ('Dataset Correlation Matrix Of Values')
-print ('------------------------------------')
-print (iris_df.corr())
-print ('====================================')
-print ('Dataset Covariance Matrix Of Values')
-print ('------------------------------------')
-print (iris_df.cov())
-print ('===============')
+print('===================================')
+print('Statistical Summary of numeric data')
+print('-----------------------------------')
+print(iris_df.describe())
+print('=======================================')
+print('Statistical Summary of non-numeric data')
+print('---------------------------------------')
+print(iris_df.describe(include=[object]))
+print('===============')
+print('Dataset Median')
+print('--------------')
+print(iris_df.median())
+print('===============')
+print('Dataset Mode')
+print('--------------')
+print(iris_df.mode(numeric_only=True))
+print('===============')
+print('Dataset Variance')
+print('--------------')
+print(iris_df.var())
+print('====================================')
+print('Dataset Correlation Matrix Of Values')
+print('------------------------------------')
+print(iris_df.corr())
+print('====================================')
+print('Dataset Covariance Matrix Of Values')
+print('------------------------------------')
+print(iris_df.cov())
+print('===============')
 
-# The next stage of the project moves on to creating histograms and scattareplots
+# The next stage of the project moves on to creating histograms and scatterplots
 a = 'sepal length in cm'
 b = 'sepal width in cm'
 c = 'petal length in cm'
@@ -79,6 +80,7 @@ e = 'class'
 
 # Creates a histogram of sepal length
 sns.set_style('darkgrid')
+plt.figure(figsize=(4.4,2.8))
 sns.distplot(iris_df[a])
 plt.title('Histogram of Sepal Length')
 plt.xlabel('')
@@ -88,6 +90,7 @@ plt.close()
 
 # Creates a histogram of sepal width
 sns.set_style('darkgrid')
+plt.figure(figsize=(4.4,2.8))
 sns.distplot(iris_df[b])
 plt.title('Histogram of Sepal Width')
 plt.xlabel('')
@@ -97,6 +100,7 @@ plt.close()
 
 # Creates a histogram of petal length
 sns.set_style('darkgrid')
+plt.figure(figsize=(4.4,2.8))
 sns.distplot(iris_df[c])
 plt.title('Histogram of Petal Length')
 plt.xlabel('')
@@ -106,6 +110,7 @@ plt.close()
 
 # Creates a histogram of petal width
 sns.set_style('darkgrid')
+plt.figure(figsize=(4.4,2.8))
 sns.distplot(iris_df[c])
 plt.title('Histogram of Petal Width')
 plt.xlabel('')
@@ -113,21 +118,32 @@ plt.ylabel('Frequency')
 plt.savefig('histogram_of_petal_width')
 plt.close()
 
+# Create a pairplot of the dataset
+sns.set_style('darkgrid')
+sns.pairplot(iris_df, hue='class')
+plt.suptitle('Iris Dataset Pairplot', size=28)
+plt.savefig('iris dataset pairplot')
+plt.close()
+
+# Create scatterplots of the dataset
+sns.set_style('darkgrid')
+sns.scatterplot(x='sepal length in cm', y='sepal width in cm',
+                data=iris_df, hue='class', style='class')
+plt.title('Scatter plot between sepal length and sepal width')
+plt.savefig('sepal scatterplot')
+plt.close()
+
+sns.set_style('darkgrid')
+sns.scatterplot(x='petal length in cm', y='petal width in cm',
+                data=iris_df, hue='class', style='class')
+plt.title('Scatter plot between petal length and petal width')
+plt.savefig('petal scatterplot')
+plt.close()
 
 
-#plt.savefig('sepal length in cm')
-#sns.pairplot(iris_df, hue='class')
-
-#plt.show()
-
-'''
-iris_df.describe().plot(kind='area', fontsize=27, figsize = (20,8), table = True,colormap='rainbow')
-plt.xlabel ('Statistics' ,)
-plt.ylabel ('Value')
-plt.title ('General Statistics of Iris Dataset')
-plt.show()
-# k=f
-#num_bins = 5
-#n, bins, patches = plt.hist(k, num_bins, facecolor='blue', alpha=0.5)
-# plt.show()
-'''
+iris_long = pd.melt(iris_df, id_vars='class')
+sns.set_style('darkgrid')
+sns.boxplot(x='class', y='value', hue='variable', data=iris_long)
+plt.title('Boxplot of Class Attributes')
+plt.savefig('class boxplot')
+plt.close()
